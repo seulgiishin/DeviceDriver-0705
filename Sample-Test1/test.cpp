c8d8be6064610d0881f1db47f053a9b33140c283
seulgiishin <tls940807@gmail.com>
2024-07-05 09:06:58 +0900

    init repo

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,8 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "../DeviceDriverKATA/DeviceDriver.cpp"
+
+TEST(DeviceTest, DeviceDriverReadTest) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
